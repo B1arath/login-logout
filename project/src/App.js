547995f891ccde53a659de8c363a427a9dc31670
B1arath <barathsankar8787@gmail.com>
2023-12-17 14:59:30 +0530

    src updated

---
+++ project/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginSigup from './Components/LoginSigup/LoginSigup';
+
+function App() {
+  return (
+    <div>
+      <LoginSigup/>
+    </div>
+  );
+}
+
+export default App;
